ab7fcfdd7609fb33327fa7c3ee436a91b275cd95
selvan7 <777tamil777@gmail.com>
2023-07-18 15:34:59 +0530

    Github action test4

--- app/src/main/java/com/testo/githubactionaccelerator/MainActivity.kt
+++ app/src/main/java/com/testo/githubactionaccelerator/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Github Action Test3")
+					Greeting("Github Action Test4")
 				}
 			}
 		}
