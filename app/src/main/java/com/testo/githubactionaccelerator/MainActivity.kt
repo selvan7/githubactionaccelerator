5f4b976b3795920503b2bcd365d4ba47c09a5010
Tamilselvan Mathiyalagan <tamilselvanm@stgit.com>
2023-07-19 14:21:49 +0530

    Github action test6

--- app/src/main/java/com/testo/githubactionaccelerator/MainActivity.kt
+++ app/src/main/java/com/testo/githubactionaccelerator/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Github Action Test5")
+					Greeting("Github Action Test6")
 				}
 			}
 		}
