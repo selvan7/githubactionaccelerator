77e1103fa2b91075bc948aaba9ef8a4bbc878b53
TAMILSELVAN MATHIYALAGAN <tamilselvanm@stgit.com>
2023-07-21 00:36:29 +0530

    commit 8

--- app/src/main/java/com/testo/githubactionaccelerator/MainActivity.kt
+++ app/src/main/java/com/testo/githubactionaccelerator/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Github Action Test7")
+					Greeting("Github Action Test8")
 				}
 			}
 		}
