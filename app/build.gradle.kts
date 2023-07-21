d5a8ce9764a4e0914f1e0ae7962729eb4a7be68d
Tamilselvan Mathiyalagan <777tamil777@gmail.com>
2023-07-21 11:17:26 +0530

    Github action test13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,8 @@ android {
 }
 
 dependencies {
-
+	implementation("com.microsoft.appcenter:appcenter-analytics:4.4.5")
+	implementation("com.microsoft.appcenter:appcenter-crashes:4.4.5")
 	implementation("androidx.core:core-ktx:1.9.0")
 	implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
 	implementation("androidx.activity:activity-compose:1.7.2")
